0d5bab695c714c4fec5c92770e44383abf308a54
Edita Latifi <108685476+EditaLatifi@users.noreply.github.com>
2025-09-09 20:57:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 // Auth
 const authRoutes = require('./routes/auth');
@@ -52,3 +53,4 @@ app.get('/', (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
