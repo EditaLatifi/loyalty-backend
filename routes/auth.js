dfa42ba612900f673bb42a84fdeb1d6fe04324dd
Edita Latifi <108685476+EditaLatifi@users.noreply.github.com>
2025-09-11 22:38:20 +0200

    Fix bcrypt native addon loading error

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const pool = require('../config/db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
